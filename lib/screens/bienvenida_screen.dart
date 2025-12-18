20c53f484f326eb64162b2b4cbd033d3b89af365
luiscr918 <lc9107040@gmail.com>
2025-12-17 21:24:04 -0500

    primera version de bienvenida,login y registro solo vista revisar bien

--- lib/screens/bienvenida_screen.dart
+++ lib/screens/bienvenida_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_netflix/screens/login_screen.dart';
+import 'package:proyecto_netflix/screens/register_screen.dart';
 
 class BienvenidaScreen extends StatelessWidget {
   const BienvenidaScreen({super.key});
@@ -6,83 +8,111 @@ class BienvenidaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: contenedor(context),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              "https://i.pinimg.com/736x/c3/17/b6/c317b60c63bd2b554f717590c78c06b7.jpg",
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(child: Center(child: _contenido(context))),
+      ),
     );
   }
 }
 
-Widget contenedor(BuildContext context) {
+Widget _contenido(BuildContext context) {
   return Padding(
-    padding: const EdgeInsets.all(24.0),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-
-        // TÍTULO
-        const Text(
-          "Bienvenido a Netflix",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.redAccent,
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-
-        const SizedBox(height: 16),
-
-        // SUBTÍTULO
-        const Text(
-          "Películas y series al alcance de un clic.",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-          ),
-        ),
-
-        const SizedBox(height: 12),
-
-        // DESCRIPCIÓN
-        const Text(
-          "Explora contenido, descubre nuevos favoritos y disfruta una experiencia de streaming diseñada para aprender Flutter.",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white70,
-            fontSize: 14,
-          ),
-        ),
-
-        const SizedBox(height: 40),
-
-        // BOTÓN LOGIN
-        SizedBox(
-          width: double.infinity,
-          child: FilledButton.icon(
-            onPressed: () {},
-            icon: const Icon(Icons.account_circle),
-            label: const Text("Iniciar sesión"),
-          ),
-        ),
-
-        const SizedBox(height: 12),
-
-        // BOTÓN REGISTER
-        SizedBox(
-          width: double.infinity,
-          child: OutlinedButton.icon(
-            onPressed: () {},
-            icon: const Icon(Icons.add_box),
-            label: const Text("Registrarse"),
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.white,
-              side: const BorderSide(color: Colors.white),
+    padding: EdgeInsets.all(24),
+    child: Container(
+      padding: EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(199, 1, 1, 1),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min, //
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // TÍTULO
+          Text(
+            "Bienvenido a Netflix",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.redAccent,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ),
-      ],
+
+          SizedBox(height: 16),
+
+          // SUBTÍTULO
+          Text(
+            "Películas y series al alcance de un clic.",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+
+          SizedBox(height: 12),
+
+          // DESCRIPCIÓN
+          Text(
+            "Explora contenido, descubre nuevos favoritos y disfruta una experiencia de streaming diseñada para aprender Flutter.",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white70, fontSize: 14),
+          ),
+
+          SizedBox(height: 32),
+
+          // BOTÓN LOGIN
+          SizedBox(
+            width: double.infinity,
+            child: FilledButton.icon(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(
+                  Color.fromRGBO(158, 32, 32, 1),
+                ),
+              ),
+              onPressed: () => irLogin(context),
+              icon: Icon(Icons.account_circle),
+              label: Text("Iniciar sesión"),
+            ),
+          ),
+
+          SizedBox(height: 12),
+
+          // BOTÓN REGISTER
+          SizedBox(
+            width: double.infinity,
+            child: OutlinedButton.icon(
+              onPressed: () => irRegister(context),
+              icon: Icon(Icons.add_box),
+              label: Text("Registrarse"),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+                side: BorderSide(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
     ),
+  );
+}
+
+void irLogin(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => LoginScreen()),
+  );
+}
+
+void irRegister(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => RegisterScreen()),
   );
 }
