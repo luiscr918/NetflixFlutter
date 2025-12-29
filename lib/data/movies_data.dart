cee73e793f467f4988c2e5b8394286c8bbe5c977
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-12-28 20:53:39 -0500

    parte realtime

---
+++ lib/data/movies_data.dart
@@ -0,0 +1,73 @@
+final Map<String, List<Map<String, String>>> moviesData = {
+  "Populares": [
+    {
+      "titulo": "El Conjuro",
+      "descripcion": "Investigadores paranormales ayudan a una familia aterrorizada.",
+      "imagen":
+          "https://megustaelcine.com/wp-content/uploads/2024/11/POSTER-El-conjuro-692x1024.jpg",
+      "trailerUrl": ""
+    },
+    {
+      "titulo": "It",
+      "descripcion": "Un payaso demoníaco aterroriza a un pueblo.",
+      "imagen":
+          "https://upload.wikimedia.org/wikipedia/en/5/5a/It_%282017%29_poster.jpg",
+      "trailerUrl": ""
+    },
+    {
+      "titulo": "Annabelle",
+      "descripcion": "Una muñeca poseída desata el terror.",
+      "imagen":
+          "https://upload.wikimedia.org/wikipedia/en/9/90/Annabelle_film_poster.jpg",
+      "trailerUrl": ""
+    }
+  ],
+
+  "Tendencias": [
+    {
+      "titulo": "La Monja",
+      "descripcion": "Una entidad demoníaca toma forma humana.",
+      "imagen":
+          "https://es.web.img2.acsta.net/pictures/18/08/14/15/35/4396669.jpg",
+      "trailerUrl": ""
+    },
+    {
+      "titulo": "El Exorcista",
+      "descripcion": "Una joven es poseída por un demonio.",
+      "imagen":
+          "https://i.ebayimg.com/images/g/Z6QAAOSwGI9iNMOP/s-l1200.jpg",
+      "trailerUrl": ""
+    },
+    {
+      "titulo": "Siniestro",
+      "descripcion": "Un escritor descubre cintas perturbadoras.",
+      "imagen":
+          "https://media.gq.com.mx/photos/5f9f3d356125b094d54f37d1/master/w_1600%2Cc_limit/sinister-poster-redes.jpg",
+      "trailerUrl": ""
+    }
+  ],
+
+  "Nuevas Llegadas": [
+    {
+      "titulo": "Hereditary",
+      "descripcion": "Una familia descubre oscuros secretos.",
+      "imagen":
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpl4Xc3R920eCuf7j_PlUmqkEtZY9g7j61KA&s",
+      "trailerUrl": ""
+    },
+    {
+      "titulo": "El Aro",
+      "descripcion": "Una cinta maldita causa muertes.",
+      "imagen":
+          "https://es.web.img2.acsta.net/pictures/17/02/03/10/37/196315.jpg",
+      "trailerUrl": ""
+    },
+    {
+      "titulo": "Midsommar",
+      "descripcion": "Un festival se vuelve una pesadilla.",
+      "imagen":
+          "https://es.web.img3.acsta.net/pictures/19/06/21/14/22/1409242.jpg",
+      "trailerUrl": ""
+    }
+  ]
+};
